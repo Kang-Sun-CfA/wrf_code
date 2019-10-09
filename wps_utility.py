23ac98322f4c94afe8cacfddee900a7754c24ebf
Kang-Sun-CfA <kang.sun@cfa.harvard.edu>
2019-10-09 15:39:07 -0400

    add F_2urbanFuel in wps

--- wps_utility.py
+++ wps_utility.py
@@ -16,6 +16,71 @@ import os
 #https://gis.stackexchange.com/questions/271226/up-sampling-increasing-resolution-raster-image-using-gdal
 # function examples
 # https://www.programcreek.com/python/example/101827/gdal.RasterizeLayer
+
+def F_2urbanFuel(shp_path, tiff_path_LF, tiff_path_out,
+                 upsampling, urban_value=12, build_value=11):
+    # This function will do the whole process of creating a 2-part urban fuel
+    # (building vs roads) at once.
+    
+    # shp_path: path to shapefile of buildings
+    # tiff_path_LF: path to downloaded tiff file from LandFire
+    # tiff_path_out: path to the output
+    # upsampling: the factor used for upsampling
+    # urban_value: value assigned to non-burning part of urban area (e.g. roads)
+    # build_value: value assigned to burning part of urban area (i.e. buildings)
+    
+    
+    # path1 is the up-sampled tif file of LandFire input file.
+    # The value corresponding to roads will be specified by user, otherwise 12.
+    # The file in path1 will be deleted eventually.
+    path1 = tiff_path_out;
+#    path1 = path1[0:path1.rfind('\\'):] + '\\AA.tif';
+    path1 = os.path.splitext(path1)[0] + '_path1.tif';
+    F_upsample_tif(tiff_path_LF, path1, upsampling, urban_value)
+    
+    
+    # path2 is a tif file containing two values:
+    # 1- 'burn_value' for detected buildings    2- 0 for anything else
+    path2 = tiff_path_out;
+#    path2 = path2[0:path2.rfind('\\'):] + '\\BB.tif';
+    path2 = os.path.splitext(path2)[0] + '_path2.tif';
+    F_shp2geotiff(shp_path,path2,path1,build_value)
+    
+    
+    # Now, all to do is fusing the two tif files:
+    U_ds = gdal.Open(path1)
+    U_band = U_ds.GetRasterBand(1)
+    dU = U_band.ReadAsArray()
+    
+    B_ds = gdal.Open(path2)
+    B_band = B_ds.GetRasterBand(1)
+    dB = B_band.ReadAsArray()
+
+
+    b = np.where(dB!=0)
+    dU[b[0],b[1]] = build_value# value for buildings
+
+
+    # Create the output tif file
+    gtiff_driver = gdal.GetDriverByName('GTiff')
+    out_ds = gtiff_driver.Create(tiff_path_out, U_band.XSize, U_band.YSize)
+    out_ds.SetProjection(U_ds.GetProjection())
+    geotransform = list(U_ds.GetGeoTransform())
+    out_ds.SetGeoTransform(geotransform)
+    out_band = out_ds.GetRasterBand(1)
+    out_band.WriteArray(dU)
+    out_band.FlushCache()
+    out_band.ComputeStatistics(False)
+    out_ds.BuildOverviews('average', [2, 4, 8, 16, 32, 64])
+    
+    
+    
+    # Now, we simply remove the intermediate files:
+    U_ds = None
+    B_ds = None
+    os.remove(path1)
+    os.remove(path2)
+
 def F_upsample_tif(tif_path,new_tif_path,upsample_factor=3):
     """
     updsample tif and replace some values
@@ -171,6 +236,44 @@ def F_reproject_shp(shp_path_in,shp_path_out,spatial_ref_out):
     # Save and close the shapefiles
     inDataSet = None
     outDataSet = None
+
+def F_geotiff2nc(fn,if_delete_tif=False):
+    """
+    converting geotiff file to netcdf. GMTED2010 is supported
+    dem_dir:
+        directory containing DEM data
+    required packages:
+        gdal, netcdf4
+    """
+    import gdal
+    from netCDF4 import Dataset
+        #print(fn)
+    f = gdal.Open(fn, gdal.GA_ReadOnly)
+    f_width = f.RasterXSize
+    f_height = f.RasterYSize
+    #f_nbands = f.RasterCount
+    f_data = f.ReadAsArray()
+    f_trans = f.GetGeoTransform()
+    xres = f_trans[1]
+    yres = f_trans[5]
+    xorig = f_trans[0]
+    yorig = f_trans[3]
+    xgrid = xorig+np.arange(0,f_width)*xres
+    ygrid = yorig+np.arange(0,f_height)*yres
+        #plt.pcolormesh(xgrid[0:3000],ygrid[0:5000],np.float16(f_data[0:5000,0:3000]))
+    f.FlushCache
+    nc_fn = os.path.splitext(fn)[0]+'.nc'
+    nc = Dataset(nc_fn,'w',format='NETCDF4_CLASSIC')
+    nc.description = 'netcdf file saved from '+fn
+    nc.createDimension('lon',len(xgrid))
+    nc.createDimension('lat',len(ygrid))
+    lon = nc.createVariable('xgrid','f8',('lon'))
+    lat = nc.createVariable('ygrid','f8',('lat'))
+    elev = nc.createVariable('z','int16',('lat','lon'))
+    lon[:] = xgrid
+    lat[:] = ygrid
+    elev[:] = f_data
+    nc.close()
     
 # test above functions
 #shp_path_in = '/mnt/Data2/GIS_data/camp_perimeter/ca_camp_20181121_2105_dd83.shp'
